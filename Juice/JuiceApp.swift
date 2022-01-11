169b1ec27e97d8e330d9c066f7604f456c715537
Matthew Guo <mguo22@gmail.com>
2022-01-11 13:00:58 -0500

    Initial Commit

---
+++ Juice/JuiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuiceApp.swift
+//  Juice
+//
+//  Created by Matthew Guo on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct JuiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
